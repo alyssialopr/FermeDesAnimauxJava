85a3be50a2c739d96cb18b21f497d177e2ed5bc1
alyssialopr <alyssia.pradon@gmail.com>
2026-09-15 16:11:22 +0200

    FIN VACHE

--- src/laFerme/Vache.java
+++ src/laFerme/Vache.java
@@ -7,6 +7,7 @@ public class Vache implements Animal {
     String nom;
     String race;
     String couleur;
+    EtatAnimal etat = EtatAnimal.LIBRE; // par defaut
     String enclos;
 
     public Vache(String nom, String race, String couleur, String enclos){
@@ -15,7 +16,46 @@ public class Vache implements Animal {
         this.race = race;
         this.couleur = couleur;
         this.enclos = enclos;
-        //this.etat = EtatAnimal.LIBRE;
+        this.etat = EtatAnimal.LIBRE;
     }
 
+    public void acheter(){
+        setEtat(EtatAnimal.LIBRE);
+    }
+
+    public void vendre(){
+        setEtat(EtatAnimal.VENDU);
+    }
+
+    public void mourir(){
+        setEtat(EtatAnimal.MORT);
+    }
+
+    public void naitre(){
+        setEtat(EtatAnimal.LIBRE);
+    }
+
+    public void nourrir(){
+        System.out.println(nom + " a été nourrie.");
+    }
+
+    public void soigner(){
+        System.out.println(nom + " a été soignée.");
+    }
+
+    public void allerEnBalade(){
+        System.out.println(nom + " part en balade.");
+    }
+
+    public EtatAnimal getEtat() {
+        return etat;
+    }
+
+    public void setEtat(EtatAnimal etat){
+        this.etat = etat;
+    }
+
+    public void setEnclos(String enclos) {
+        this.enclos = enclos;
+    }
 }
