3b48f7f6eb46505dea6ef7eb84e3d2d49490cf39
alyssialopr <alyssia.pradon@gmail.com>
2026-09-15 14:59:36 +0200

    document + eleveur

---
+++ src/laFerme/Document.java
@@ -0,0 +1,11 @@
+package laFerme;
+
+public interface Document {
+
+    void nourrir();
+    void allerEnBalade();
+    void vendre();
+    void naitre();
+    void mourir();
+    void soigner();
+}
---
+++ src/laFerme/Eleveur.java
@@ -0,0 +1,8 @@
+package laFerme;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Eleveur {
+    private static int CPT = 1;
+}
